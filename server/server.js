820284b4032ce21e524596ab5476c803be6f81c3
Komal <komal22124@gmail.com>
2025-06-29 03:19:11 +0530

    routing done.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import http from 'http';
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 //create express app and http server
 const app = express();
@@ -11,7 +13,11 @@ const server = http.createServer(app);
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+//Route setup
 app.use("/api/status", (req, res) => res.send("Server is live!"));
+app.use("/api/auth", userRouter);
+//connect to mongodb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
