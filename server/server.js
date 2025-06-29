f980fb6baabfcd55a25bbbd304dc6a10517c6ec8
Komal <komal22124@gmail.com>
2025-06-29 20:31:10 +0530

    Backend completed.

--- server/server.js
+++ server/server.js
@@ -4,10 +4,41 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRouter.js";
+import {Server} from "socket.io";
+import { log } from "console";
 
 //create express app and http server
 const app = express();
 const server = http.createServer(app);
+
+//intiallize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//store online users
+export const userSocketMap = {};// { userId: socketId }
+
+//Socket.io connectionhandler
+io.on("connection", (socket) => {
+    const userId= socket.hanshake.quesry.userId;
+    console.log("User connected" , userId);
+
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+
+    //emit online users to all clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", userId);
+        delete userSocketMap[userId];
+        //emit online users to all clients
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
 
 //middleware setup  
 app.use(express.json({limit: "4mb"}));
@@ -16,6 +47,8 @@ app.use(cors());
 //Route setup
 app.use("/api/status", (req, res) => res.send("Server is live!"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages" , messageRouter);
+
 //connect to mongodb
 await connectDB();
 
